4659ec72bae60129ac7bd3ade1a8797546b7dd82
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-22 10:25:11 +0530

    Update Substring.sh

--- Substring.sh
+++ Substring.sh
@@ -8,3 +8,30 @@ then
 echo “a is equal to b”
 else
 echo “a is not equal to b”
+fi
+a=” ”
+if[-z $a]
+then
+echo “string length is zero”
+else
+echo “string length is non zero”
+fi
+if[$a]
+then
+echo “string is not empty”
+else 
+echo “string is empty”
+fi
+if[-n $a]
+then
+echo “string is not zero”
+else
+echo “string length is zero”
+fiif[$a!=$b]
+then
+echo “a is not equal to b”
+else 
+echo “a is equal to b”
+fi
+s=”sample string”
+echo ${#s}
