db40b43eed673307a349cfcc129942506bb0a920
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-22 10:49:00 +0530

    Update Upper2Lower.sh

--- Upper2Lower.sh
+++ Upper2Lower.sh
@@ -1,8 +1,25 @@
-for i in vishnu
-do 
-echo “before conversion is”
-echo $i;
-j=`echo $i|tr’[a-z]’’[A-Z]’`
-echo “after conversion is”
-echo $j;
+
+
+
+
+
+
+
+
+for i in *
+
+do
+
+echo Before Converting to uppercase the filename is
+
+echo $i
+
+j=`echo $i | tr '[a-z]' '[A-Z]'`
+
+echo After Converting to uppercase the filename is
+
+echo $j
+
+mv $i $j
+
 done
