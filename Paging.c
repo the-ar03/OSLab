a1e5c2a96e947a61e802e78533ceb247b5d7fc45
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-22 09:32:28 +0530

    Create Paging.c

---
+++ Paging.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+#include<math.h>
+
+main()
+
+{
+
+int size,m,n,pgno,pagetable[3]={5,6,7},i,j,frameno;
+
+double m1;
+
+int ra=0,ofs;
+
+clrscr();
+
+printf("enter process size:");
+
+scanf("%d",&size);
+
+m1=size/4;
+
+n=ceil(m1);
+
+printf("total no. of pages%d",n)
+
+printf("\n enter relative address \n:")]
+
+prscanf("%d",&ra);
+
+pgno=ra/1000;
+
+ofs=ra%1000;
+
+printf("pageno=%d\n",pgno);
+
+printf("page table");
+
+for(i=0;i<n;i++)
+
+printf("\n %d [%d]",i,pagetable[i]);
+
+frameno=pagetable[pgno];
+
+printf("\n equivalent physical address:%d%d",frameno,ofs);
+
+getch();
+
+}
