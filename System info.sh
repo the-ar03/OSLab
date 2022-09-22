fa0b8a5a322ed992f2d7b4d7ed6fd4bb5b6f2e49
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-22 10:46:42 +0530

    Update System info.sh

--- System info.sh
+++ System info.sh
@@ -1,5 +1,25 @@
-echo "NETWORK = $(uname –n)"
-echo "Kernel NAME = $(uname –s)"
-echo "Kernel version = $(uname –v)"
-echo "Operating System = $(uname –m)"
-echo "All information = $(uname –a)"
+echo "SYSTEM INFORMATION"
+
+echo “Hello ,$LOGNAME”
+
+echo “Current Date is = $(date)”
+
+echo “User is ‘who I am’”
+
+echo “Current Directory = $(pwd)”
+
+echo "Network Name and Node Name = $(uname -n)"
+
+echo "Kernal Name =$(uname -s)"
+
+echo "Kernal Version=$(uname -v)"
+
+echo "Kernal Release =$(uname -r)"
+
+echo "Kernal OS =$(uname -o)"
+
+echo “Proessor Type = $(uname -p)”
+
+echo “Kernel Machine Information = $(uname –m)”
+
+echo "All Information =$(uname -a)"
